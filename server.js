04cd469fdd592f169126fe59d198695391839e2d
Smart Dev <danieldev0612@gmail.com>
2023-03-19 16:00:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const path = require('path')
+const express = require('express')
+
+const PORT = 3000
+
+const app = express()
+
+app.use('/secret', (req, res) => {
+  return res.send('Your personal secret value is 42!')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
