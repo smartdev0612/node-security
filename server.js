744ac9e41ec4a60b0379b52fa1ecc490149d609e
Smart Dev <danieldev0612@gmail.com>
2023-03-20 02:06:25 -0700

    setup oauth2.0

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const http = require('http')
+const fs = require('fs')
 const path = require('path')
+const https = require('https')
 const express = require('express')
+const helmet = require('helmet')
 
 const PORT = 3000
 
 const app = express()
 
-app.use('/secret', (req, res) => {
+app.use(helmet())
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: 'You must log in!',
+    })
+  }
+  next()
+}
+
+app.get('/auth/google', (req, res) => {})
+
+app.get('/auth/google/callback', (req, res) => {})
+
+app.get('/auth/logout', (req, res) => {})
+
+app.use('/secret', checkLoggedIn, checkPermissions, (req, res) => {
   return res.send('Your personal secret value is 42!')
 })
 
@@ -14,6 +34,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
